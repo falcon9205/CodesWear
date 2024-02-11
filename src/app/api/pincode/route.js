63277b2c8afc335725a6cbe37a0cab065123889f
Himanshu Maurya <mauryahimanshu567@gmail.com>
2024-02-12 01:43:45 +0530

    redefine-modules

--- src/app/api/pincode/route.js
+++
@@ -1,5 +0,0 @@
-import { NextResponse } from "next/server";
-export async function GET(req){
-    const pincode = ["110084","122033","110009","712002","110034","110094","110034","110042","110005","110023","110009"]
-    return NextResponse.json({pincode})
-}
